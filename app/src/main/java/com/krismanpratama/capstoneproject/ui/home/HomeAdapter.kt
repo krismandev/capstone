a1c6c3527114ee17ef51a44e8708cf28f2033888
krismandev <krismanpratama@gmail.com>
2021-05-08 08:44:07 +0700

    first commit

---
+++ app/src/main/java/com/krismanpratama/capstoneproject/ui/home/HomeAdapter.kt
@@ -0,0 +1,24 @@
+package com.krismanpratama.capstoneproject.ui.home
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.krismanpratama.capstoneproject.databinding.ItemCctvActiveRowBinding
+
+class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+
+    inner class HomeViewHolder(private val binding: ItemCctvActiveRowBinding) :RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
